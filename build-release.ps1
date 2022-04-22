e3d2df59eb4a7e67b471c839deade017f2cb0d52
shikatan0 <shikata713@gmail.com>
2022-04-23 05:33:06 +0900

    u

--- build-release.ps1
+++ build-release.ps1
@@ -1,4 +1,4 @@
 esbuild src/index.ts --outdir=out --minify --bundle --format=esm --log-level=warning
-sass src/index.sass out/index.css
+sass src/index.sass out/index.css --no-source-map
 postcss out/index.css -o out/index.css -u autoprefixer cssnano --no-map
 pug src/index.pug -o docs
